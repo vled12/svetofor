9da5e9d37137c2759f41345807392c2cf8720683
vled12 <vleduser@gmail.com>
2017-02-11 00:58:56 +0300

    add time correction add delete function add interval input

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.properties import StringProperty
 from kivy.properties import ObjectProperty
 from kivy.properties import ListProperty
+from kivy.properties import BooleanProperty
 from svetofor import Svetofor
 
 from kivy.clock import Clock
@@ -14,28 +15,37 @@ class Light(GridLayout):
     label = StringProperty('')
     time_left = StringProperty('')
     color=ListProperty()
-    def __init__(self, address, **kwargs):
+    killme=BooleanProperty(False)
+    killme2=ObjectProperty(None)
+    def __init__(self, address, red_period=10.0, gr_period=5.0, **kwargs):
         super(Light,self).__init__(**kwargs)
         self.label=address
-        self.light=Svetofor()
+        self.light=Svetofor(red_period,gr_period)
         self.color=([0,0,0,1])
         Clock.schedule_interval(self.update, 0.1)
 
     def update(self,dt):
         self.light.check()
         self.time_left='{:{width}.{prec}f}'.format(self.light.time_left, width=2, prec=1)
-        self.color[0]=0.0 if self.light.status else 1.0
+        self.color[0]= 0.0 if self.light.status else 1.0
         self.color[1] = 1.0 if self.light.status else 0.0
 
+    def correct_rg(self):
+        self.light.correct_rg()
+    def correct_gr(self):
+        self.light.correct_gr()
+    def die(self):
+        self.parent.remove_widget(self)
 
 class StartScreen(Widget):
     def __init__(self, **kwargs):
         self.lightsView = ObjectProperty(None)
         self.entered_address=ObjectProperty()
+        self.gr_period = ObjectProperty()
+        self.red_period=ObjectProperty()
         super(StartScreen, self).__init__(**kwargs)
     def add_svetofor(self):
-        self.lightsView.add_widget(Light(self.entered_address.text))
-
+        self.lightsView.add_widget(Light(self.entered_address.text,float(self.gr_period.text),float(self.red_period.text)))
 class LightsApp(App):
     def build(self):
         return StartScreen()
--- svetofor.py
+++ svetofor.py
@@ -1,23 +1,31 @@
-#-*-coding:utf8;-*-
-#qpy:console
-#qpy:2
+# -*-coding:utf8;-*-
+# qpy:console
+# qpy:2
 
 import time, math
 
 print time.time()
 
+
 class Svetofor():
-  time_left=0.0
-  def __init__(self):
-    self.status=True
-    self.red2green_fixed=time.time()
-    self.red_interval=5.0
-    self.green_interval=10.0
-    self.cycle= self.green_interval+self.red_interval
-    
-  def check(self):
-    cycles=int(math.floor((time.time()-self.red2green_fixed)/(self.cycle)))
-    since_last= time.time()-cycles*self.cycle-self.red2green_fixed
-    self.status= (since_last)<self.green_interval
-    self.time_left=self.status and self.green_interval-since_last or self.cycle-since_last
-    return self.status
+    time_left = 0.0
+
+    def __init__(self, red_interval=5.0, green_interval=10.0):
+        self.status = True
+        self.red2green_fixed = time.time()
+        self.red_interval = red_interval
+        self.green_interval = green_interval
+        self.cycle = self.green_interval + self.red_interval
+
+    def check(self):
+        cycles = int(math.floor((time.time() - self.red2green_fixed) / (self.cycle)))
+        since_last = time.time() - cycles * self.cycle - self.red2green_fixed
+        self.status = (since_last) < self.green_interval
+        self.time_left = self.status and self.green_interval - since_last or self.cycle - since_last
+        return self.status
+
+    def correct_rg(self):
+        self.red2green_fixed = time.time()
+
+    def correct_gr(self):
+        self.red2green_fixed = time.time() - self.red_interval
