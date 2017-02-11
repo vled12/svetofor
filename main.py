f63ed76c5ef7b1be1257ac449c944d35c47b36cb
vled12 <vleduser@gmail.com>
2017-02-12 01:01:35 +0300

    add save/restore functionabilty (lot of bugs)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import kivy
+import time
 kivy.require('1.1.1')
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -17,10 +18,10 @@ class Light(GridLayout):
     color=ListProperty()
     killme=BooleanProperty(False)
     killme2=ObjectProperty(None)
-    def __init__(self, address, red_period=10.0, gr_period=5.0, **kwargs):
+    def __init__(self, address, red_period=10.0, gr_period=5.0, red2green_fixed=time.time(),**kwargs):
         super(Light,self).__init__(**kwargs)
         self.label=address
-        self.light=Svetofor(red_period,gr_period)
+        self.light=Svetofor(red_period,gr_period,red2green_fixed)
         self.color=([0,0,0,1])
         Clock.schedule_interval(self.update, 0.1)
 
@@ -44,8 +45,25 @@ class StartScreen(Widget):
         self.gr_period = ObjectProperty()
         self.red_period=ObjectProperty()
         super(StartScreen, self).__init__(**kwargs)
+        try:
+            memory=open('memory','r')
+            for item in memory.readlines():
+                data=item.split(';')
+                data[1:]=map(float,data[1:])
+                print data
+                self.lightsView.add_widget(Light(data[0],data[1],data[2],data[3]))
+        except: pass
+
+
+
     def add_svetofor(self):
         self.lightsView.add_widget(Light(self.entered_address.text,float(self.gr_period.text),float(self.red_period.text)))
+
+    def save(self):
+        with open("memory",'w+') as memory:
+            for light in self.lightsView.children:
+                memory.write(';'.join([light.label,light.light.export()]))
+
 class LightsApp(App):
     def build(self):
         return StartScreen()
--- svetofor.py
+++ svetofor.py
@@ -4,18 +4,15 @@
 
 import time, math
 
-print time.time()
-
-
 class Svetofor():
     time_left = 0.0
 
-    def __init__(self, red_interval=5.0, green_interval=10.0):
-        self.status = True
-        self.red2green_fixed = time.time()
+    def __init__(self, red_interval=5.0, green_interval=10.0, red2green_fixed=time.time()):
+        self.red2green_fixed = red2green_fixed
         self.red_interval = red_interval
         self.green_interval = green_interval
         self.cycle = self.green_interval + self.red_interval
+        print locals()
 
     def check(self):
         cycles = int(math.floor((time.time() - self.red2green_fixed) / (self.cycle)))
@@ -29,3 +26,6 @@ class Svetofor():
 
     def correct_gr(self):
         self.red2green_fixed = time.time() - self.red_interval
+
+    def export(self):
+        return ';'.join(map(str,[self.red_interval,self.red_interval,self.red2green_fixed]))
